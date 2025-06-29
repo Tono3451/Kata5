7b8baaafc8d7ac4fe3e5f0444a8ff4d97a02832e
Tono3451 <116306863+Tono3451@users.noreply.github.com>
2025-06-29 06:30:14 +0100

    added dependencies and improve stringToImageIcon to support URL

--- src/main/java/software/ulpgc/kata5/control/EnterNewPokemonCommand.java
+++ src/main/java/software/ulpgc/kata5/control/EnterNewPokemonCommand.java
@@ -2,8 +2,13 @@ package software.ulpgc.kata5.control;
 
 import software.ulpgc.kata5.view.MainFrame;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+
+import static com.google.gson.internal.bind.TypeAdapters.URI;
 
 public class EnterNewPokemonCommand implements Command {
     private MainFrame mainFrame;
@@ -16,19 +21,20 @@ public class EnterNewPokemonCommand implements Command {
     public void execute() {
         /*
         String pokemon = mainFrame.getTextInput();
-        mainFrame.updatePokemonInfo(pokemon, stringToImageIcon());
+        mainFrame.updatePokemonInfo(pokemon, stringToImageIcon("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"));
          */
     }
 
-    private ImageIcon stringToImageIcon(String png) {
+    private ImageIcon stringToImageIcon(String imageUrl) {
         try {
-            ImageIcon originalIcon = new ImageIcon(png);
-            if (originalIcon.getIconWidth() == -1) {
+            URL url = new URL(imageUrl);
+            BufferedImage bufferedImage = ImageIO.read(url);
+
+            if (bufferedImage == null) {
                 return null;
             }
 
-            Image image = originalIcon.getImage();
-            Image scaledImage = image.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+            Image scaledImage = bufferedImage.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
             return new ImageIcon(scaledImage);
         } catch (Exception e) {
             e.printStackTrace();
